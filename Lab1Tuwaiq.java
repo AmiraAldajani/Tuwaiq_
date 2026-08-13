5c01e2cb6b90cf2fd802134e9ef1785cac2dd265
Amira Mohammed Aldajani <85336248+AmiraAldajani@users.noreply.github.com>
2026-08-13 14:23:27 +0300

    Add files via upload

---
+++ Lab1Tuwaiq.java
@@ -0,0 +1,218 @@
+
+import java.util.Scanner;
+    void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+
+        //Q1
+        System.out.print("Plsea enter two numbers.");
+        System.out.print("Number one: ");
+        int num1 = s.nextInt();
+        System.out.print("Number two: ");
+        int num2 = s.nextInt();
+        int sum, multibly, subt, divide, remainder;
+        sum = num1+num2;
+        multibly = num1*num2;
+        subt = num1-num2;
+        divide = num1/num2;
+        remainder = num1%num2;
+        System.out.println(num1+"+"+num2+" = "+sum);
+        System.out.println(num1+"-"+num2+" = "+subt);
+        System.out.println(num1+"x"+num2+" = "+multibly);
+        System.out.println(num1+"/"+num2+" = "+divide);
+        System.out.println(num1+" mod "+num2+" = "+remainder);
+
+
+        //Q2
+        System.out.print("Input a number to get its multiplication table: ");
+        int numM = s.nextInt();
+        for (int i= 1; i <=10; i++){
+            System.out.println(numM+"x"+i+" = "+numM*i);
+        }
+
+
+        //Q3
+        System.out.print("Enter Redius: ");
+        double redius = s.nextDouble();
+        double area = 3.14*(redius*redius);
+        double perimeter = 2*3.14*redius;
+        System.out.println("Perimeter is: "+perimeter);
+        System.out.println("Area is: "+ area);
+
+
+        //Q4
+        System.out.print("Enter the count of numbers: ");
+        int counter = s.nextInt();
+        int sumNum=0, input;
+        for(int i=0; i<counter; i++){
+            System.out.print("Enter an integer: ");
+            input = s.nextInt();
+            sumNum = sumNum + input;
+        }
+        double average = (double)sumNum / counter;
+        System.out.println("The average is: "+ average);
+
+
+        //Q5
+        System.out.print("Enter the first number: ");
+        int firstNum = s.nextInt();
+        System.out.print("Enter the second number: ");
+        int secNum = s.nextInt();
+        System.out.print("Enter the third number: ");
+        int thirdNum = s.nextInt();
+        if (firstNum+secNum == thirdNum) System.out.print("Result is true");
+        else System.out.println("Result is false");
+
+
+        //Q6
+        s.nextLine();
+        System.out.print("Enter a word: ");
+        String word = s.nextLine();
+        for( int i=0; i < word.length(); i++){
+            System.out.print(word.charAt(word.length()-1-i));
+        }
+        System.out.println("");
+
+
+        //Q7
+        System.out.print("Enter a number to check if even or odd: ");
+        int userNum = s.nextInt();
+        if ( userNum%2 ==0 ) System.out.println("Is even");
+        else System.out.println("Is odd");
+
+
+        //Q8
+        System.out.print("Enter temp in Centigrade: ");
+        double centigrade = s.nextInt();
+        double fehrenheit = centigrade* 9/5 + 32;
+        System.out.println("Temperature in Fehrenheit is "+ fehrenheit);
+
+
+        //Q9
+        System.out.print("Enter a string: ");
+        s.nextLine();
+        String userString = s.nextLine();
+        System.out.print("Enter an Index: ");
+        int indexString = s.nextInt();
+        System.out.println(userString.charAt(indexString));
+
+
+        //Q10
+        System.out.print("Enter Width: ");
+        double width = s.nextDouble();
+        System.out.print("Enter Height: ");
+        double height = s.nextDouble();
+        double areaRectangle = width*height;
+        double perimeterRectangle = 2* (width+height);
+        System.out.println("Perimeter is: "+perimeterRectangle);
+        System.out.println("Area is: "+ areaRectangle);
+
+
+        //Q11
+        System.out.print("Enter input1: ");
+        int input1 = s.nextInt();
+        System.out.print("Enter input2: ");
+        int input2 = s.nextInt();
+        if(input1 == input2) System.out.println(input1+"=="+input2);
+        else System.out.println(input1+"!="+input2);
+        if(input1 < input2 && input1 != input2 ) System.out.println(input1+" < "+input2);
+        else System.out.println(input1+" > "+input2);
+        if(input1 <= input2) System.out.println(input1+" <= "+input2);
+        if(input1 >= input2) System.out.println(input1+" >= "+input2);
+
+
+
+        //Q12
+        System.out.print("Enter seconds: ");
+        int seconds = s.nextInt();
+        int hour=0, minute=0, second=0;
+        for (int i =0; i <seconds; i++){
+            second++;
+            if ( second== 60) {
+                second = 0;
+                minute++;
+            }
+            if ( minute== 60) {
+                minute = 0;
+                hour++;}
+        }
+        System.out.println(hour+":"+minute+":"+second);
+
+
+        //Q13
+        System.out.print("Enter first number: ");
+        int value1 = s.nextInt();
+        System.out.print("Enter second number: ");
+        int value2 = s.nextInt();
+        System.out.print("Enter third number: ");
+        int value3 = s.nextInt();
+        System.out.print("Enter forth number: ");
+        int value4 = s.nextInt();
+        if ( value1 == value2 && value3 == value4 && value2 == value3) System.out.println("Numbers are equal!");
+        else System.out.println("Numbers are NOT equal.");
+
+        //Q14
+        System.out.println("Enter a number: ");
+        int numToBeChecked = s.nextInt();
+        if (numToBeChecked == 0) System.out.println("Number is zero.");
+        else if (numToBeChecked > 0) System.out.println("Number is positive.");
+        else System.out.println("Number is negative");
+
+
+        //Q15
+        System.out.print("Enter numbers and stop by entering -1: ");
+        int userInput_ = s.nextInt();
+        int positive=0, negative=0,zero=0;
+        while(userInput_ != -1){
+            if (userInput_ == 0) zero++;
+            else if (userInput_ > 0) positive++;
+            else negative++;
+            System.out.print("Enter another (stop by entering -1): ");
+            userInput_ = s.nextInt();
+        }
+        System.out.println("Positives: "+positive+"\nNigatives: "+negative+"\nZeros: "+zero);
+
+
+        //Q16
+        System.out.print("Enter a long-ish number: ");
+        int numberToBeReverse = s.nextInt();
+        String numberToBeReversed = ""+ numberToBeReverse;
+        for( int i=0; i < numberToBeReversed.length(); i++){
+            System.out.println(numberToBeReversed.charAt(numberToBeReversed.length()-1-i));}
+
+
+        //Q17
+        int input_1, input_2, input_repeat, smallNumber, largeNumber;
+        System.out.print("Enter AT LEAST two numbers: ");
+        input_1 = s.nextInt();
+        System.out.print("Enter another: ");
+        input_2 = s.nextInt();
+        if (input_1 >= input_2) {
+            largeNumber = input_1;
+            smallNumber = input_2;
+        }
+        else {
+            largeNumber = input_2;
+            smallNumber = input_1;
+        }
+        input_repeat = input_2;
+        while (input_repeat != 0){
+            if (input_repeat > largeNumber) largeNumber = input_repeat;
+            if (input_repeat < smallNumber) smallNumber = input_repeat;
+            System.out.print("Enter another (0 to stop): ");
+            input_repeat = s.nextInt();
+        }
+        System.out.println("the large number: "+largeNumber);
+        System.out.println("the small number: "+smallNumber);
+
+
+        //Q18
+        System.out.print("Enter string (counting how many 'a' there is): ");
+        int count_ = 0;
+        String user_string = s.nextLine();
+        for (int i =0; i<user_string.length(); i++){
+            if(user_string.charAt(i)=='a' || user_string.charAt(i)=='A') count_++;
+        }
+        System.out.println("Number of a's is: "+count_);
+
+}
