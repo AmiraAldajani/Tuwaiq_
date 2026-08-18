72f515e09665e8182240716f98c2ba579b875fdb
Amira Mohammed Aldajani <85336248+AmiraAldajani@users.noreply.github.com>
2026-08-18 22:48:38 +0300

    Update Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Random;
 public class Lab2 {
 
     public static void main(String[] args) {
@@ -44,7 +46,7 @@ public class Lab2 {
         System.out.println("QUESTION 2 ===================================================");
         int[] arrayInt = { 1, 1,1,3,3,5};
         ArrayList<Integer> values = new ArrayList<>();
-        int value, accurs=0;
+        int value, accurs;
         for ( int i=0; i< arrayInt.length; i++){
             value = arrayInt[i]; accurs =0;
             if (!values.contains(value)){ values.add(value);
@@ -57,16 +59,55 @@ public class Lab2 {
 
         //Q3
         System.out.println("QUESTION 3 ===================================================");
+        System.out.print("Please enter the amount of numbers to find: ");
+        int k = s.nextInt();
+        int[] numArray = {1,4,17,7,25,3,100};
+        Arrays.sort(numArray);
+        for (int i =numArray.length-1; i>= numArray.length-k; i--) {
+            System.out.print(numArray[i]+" ");
+        }
+        System.out.println();
 
 
-        //Q4
+        //Q4 this question was too hard
         System.out.println("QUESTION 4 ===================================================");
+        int[] numArray2 = {5,4,3,2,1};
+        reverse(numArray2, 0, numArray2.length-1);
+        for (int i=0; i <numArray2.length; i++) {
+            System.out.print(numArray2[i] + " ");
+        }
+        System.out.println();
 
         //Q5
         System.out.println("QUESTION 5 ===================================================");
+        System.out.print("Please pick from the following: \n1. Accept elements.\n2. Display elements.\n3. Search an element.\n4. Sort the array.\n5. Stop");
+        int choice = s.nextInt();
+        while (choice != 5){
+            switch (choice){
+                case 1:
 
+            }
+            System.out.print("Please pick from the following: \n1. Accept elements.\n2. Display elements.\n3. Search an element.\n4. Sort the array.\n5. Stop");
+            choice = s.nextInt();
+        }
+        
+        
         //Q6
         System.out.println("QUESTION 6 ===================================================");
+        int min, max, count;
+        System.out.print("Enter minimum: ");
+        min = s.nextInt();
+        System.out.print("Enter maximum: ");
+        max = s.nextInt();
+        System.out.print("Enter how many numbers to generate: ");
+        count = s.nextInt();
+        System.out.println("Random numbers within the specified range: ");
+        for (int i= 0; i< count; i++) {
+            System.out.print(getRandomNum(min, max));
+            if (i <count -1) System.out.print(" | ");
+        }
+        System.out.println();
+
 
         //Q7
         System.out.println("QUESTION 7 ===================================================");
@@ -74,5 +115,17 @@ public class Lab2 {
         //Q8
         System.out.println("QUESTION 8 ===================================================");
 
+    }
+    
+    public static void reverse( int[] i, int j, int k) {
+        if (j >= k) return;
+        int num = i[j];
+        i[j] = i[k];
+        i[k] = num;
+        reverse(i, j + 1, k - 1);
+    }
+    
+    public static int getRandomNum(int min, int max) {
+        return (int)((Math.random()* (max-min))+ min);
     }
 }
